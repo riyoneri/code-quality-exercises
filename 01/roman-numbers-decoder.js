89a24ab0af672e11baf3157cea533d23d65c8a45
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-19 11:21:27 +0200

    refactor: change variable naming in roman-numbers-decoder.js[01]

--- 01/roman-numbers-decoder.js
+++ 01/roman-numbers-decoder.js
@@ -1,5 +1,5 @@
 function romansToNumbers(string) {
-  const ROMAN_NUMBER_VALUES = new Map([
+  const romanNumberValues = new Map([
     ["I", 1],
     ["V", 5],
     ["X", 10],
@@ -13,13 +13,16 @@ function romansToNumbers(string) {
     currentNumber,
     previousNumber = 0,
     arrangedNumbers = string.split("").reverse();
+
   for (const character of arrangedNumbers) {
-    currentNumber = ROMAN_NUMBER_VALUES.get(character);
+    currentNumber = romanNumberValues.get(character);
+
     if (currentNumber >= previousNumber) {
       resultNumber += currentNumber;
     } else {
       resultNumber -= currentNumber;
     }
+    
     previousNumber = currentNumber;
   }
   return resultNumber;
